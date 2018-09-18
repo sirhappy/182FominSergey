b246cb804a46f020f547276b5b3d60f37c5c73c4
Serega <sdfomin@edu.hse.ru>
2018-09-18 16:38:31 +0300

    Task06

---
+++ 1module/Homework02/Task06/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Группа: БПИ182-2
+ * Студент: Фомин Сергей Дмитриевич
+ * Дата: 18.09.2018
+ * Вариант: -
+ * Задача: Круг R = 10. Точка внутри него или нет?
+*/
+
+using System;
+
+namespace Task06
+{
+    class Program
+    {
+        public static int ReadInt(string msg = "Введите целое число: ")
+        {
+            int a;
+            do Console.Write(msg);
+            while (!int.TryParse(Console.ReadLine(), out a));
+            return a;
+        }
+        public static double ReadDouble(string msg = "Введите вещественное число: ")
+        {
+            double a;
+            do Console.Write(msg);
+            while (!double.TryParse(Console.ReadLine(), out a));
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int x = ReadInt("Введите координату x: ");
+                int y = ReadInt("Введите координату y: ");
+                int r = 10, d = x * x + y * y;
+                if (d < r * r)
+                {
+                    Console.WriteLine("Точка лежит внутри круга");
+                }
+                else if (d == r * r)
+                {
+                    Console.WriteLine("Точка лежит на границе круга");
+                }
+                else
+                {
+                    Console.WriteLine("Точка лежит за пределами круга");
+                }
+
+                Console.WriteLine("Нажмите ESC для выхода программы. Для повторного запуска - любую другую клавишу");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
