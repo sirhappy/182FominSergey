4740acbbcddfc44c6a8139721e824555f08670cc
Serega <sdfomin@edu.hse.ru>
2018-09-18 16:48:19 +0300

    Task07

---
+++ 1module/Homework02/Task07/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Группа: БПИ182-2
+ * Студент: Фомин Сергей Дмитриевич
+ * Дата: 18.09.2018
+ * Вариант: -
+ * Задача: !(x && y || z)
+*/
+
+using System;
+
+namespace Task07
+{
+    class Program
+    {
+        public static bool ReadBool(string msg = "Введите 0 или 1: ")
+        {
+            int a;
+            do Console.Write(msg);
+            while (!int.TryParse(Console.ReadLine(), out a) || !(a == 0 || a == 1));
+            return a == 1 ? true : false;
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                bool x = ReadBool("Введите x: ");
+                bool y = ReadBool("Введите y: ");
+                bool z = ReadBool("Введите z: ");
+                Console.WriteLine($"!(x && y || z) = {!(x && y || z)}");
+
+                Console.WriteLine("Нажмите ESC для выхода программы. Для повторного запуска - любую другую клавишу");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
