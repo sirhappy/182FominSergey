8aae5c0b5d60a95caef1dc40ea039698ce27efb4
Serega <sdfomin@edu.hse.ru>
2018-09-18 15:57:55 +0300

    Homework 2, Task 05

---
+++ 1module/Homework02/Task05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    class Program
+    {
+        public static int ReadInt(string msg = "Введите инт")
+        {
+            string inputStr;
+            int a;
+            do
+            {
+                Console.Write(msg);
+                inputStr = Console.ReadLine();
+            } while (!int.TryParse(inputStr, out a) || !(a >= 100 && a <= 999));
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                int a = ReadInt("Введите трёхзначное число: ");
+                Console.WriteLine(a / 100);
+                Console.WriteLine(a / 10 % 10);
+                Console.WriteLine(a % 10);
+                Console.WriteLine("Нажмите ESC для выхода программы. Для повторного запуска - любую другую клавишу");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
