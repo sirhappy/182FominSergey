e976206fd5a83d18b7546e85bb52c28010738ae2
Serega <sdfomin@edu.hse.ru>
2018-09-18 16:14:00 +0300

    Task05

--- 1module/Homework02/Task05/Program.cs
+++ 1module/Homework02/Task05/Program.cs
@@ -14,23 +14,29 @@ namespace Task05
     {
         public static int ReadInt(string msg = "Введите инт")
         {
-            string inputStr;
             int a;
-            do
-            {
-                Console.Write(msg);
-                inputStr = Console.ReadLine();
-            } while (!int.TryParse(inputStr, out a) || !(a >= 100 && a <= 999));
+            do Console.Write(msg);
+            while (!int.TryParse(Console.ReadLine(), out a) || !(a >= 100 && a <= 999));
             return a;
         }
+
+        public static void ToDigit(int a, out int d1, out int d2, out int d3)
+        {
+            d1 = a / 100;
+            d2 = a / 10 % 10;
+            d3 = a % 10;
+        }
+
         static void Main(string[] args)
         {
             do
             {
                 int a = ReadInt("Введите трёхзначное число: ");
-                Console.WriteLine(a / 100);
-                Console.WriteLine(a / 10 % 10);
-                Console.WriteLine(a % 10);
+                ToDigit(a, out int d1, out int d2, out int d3);
+                Console.WriteLine(d1);
+                Console.WriteLine(d2);
+                Console.WriteLine(d3);
+
                 Console.WriteLine("Нажмите ESC для выхода программы. Для повторного запуска - любую другую клавишу");
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
