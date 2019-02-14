36da777d5313083e1a2ce0754aaa803f49d43ec2
Sergey Fomin <sdfomin@edu.hse.ru>
2019-02-14 14:34:23 +0300

    Самостоятельная работа 4

--- 3module/Fomin_SR4_var3/Fomin_SR4_var3/Program.cs
+++ 3module/Fomin_SR4_var3/Fomin_SR4_var3/Program.cs
@@ -10,7 +10,7 @@ namespace Fomin_SR4_var3
     {
         static void Main(string[] args)
         {
-            ConsoleKey consoleKey = ConsoleKey.A;
+            ConsoleKey consoleKey = ConsoleKey.NoName;
             do
             {
                 int a = 0, b = 0;
@@ -54,6 +54,11 @@ namespace Fomin_SR4_var3
             } while (consoleKey != ConsoleKey.Escape);
         }
 
+        /// <summary>
+        ///     Выводит информацию о квадрате
+        /// </summary>
+        /// <param name="sender">Square</param>
+        /// <param name="e">SquareEventArgs</param>
         public static void PrintInfo(object sender, SquareEventArgs e)
         {
             Square s = sender as Square;
@@ -64,18 +69,39 @@ namespace Fomin_SR4_var3
 
     class Square
     {
+        /// <summary>
+        ///     Конструктор для создания квадрата
+        /// </summary>
+        /// <param name="coordinate">Координата левой нижней точки</param>
         public Square(Coordinate coordinate)
         {
             Coordinates = coordinate;
         }
 
+        /// <summary>
+        ///     Сторона квадрата
+        /// </summary>
         public const int Side = 10;
 
+        /// <summary>
+        ///     Стандартный шаблон для события изменения координаты
+        /// </summary>
+        /// <param name="sender">Square</param>
+        /// <param name="e">SquareEventArgs с конечной точкой</param>
         public delegate void CoordinateChanged(object sender, SquareEventArgs e);
+        /// <summary>
+        ///     Событие изменения координаты
+        /// </summary>
         public static event CoordinateChanged OnCoordinateChanged;
 
+        /// <summary>
+        ///     Координата
+        /// </summary>
         private Coordinate coordinate;
 
+        /// <summary>
+        ///     Свойство для координаты
+        /// </summary>
         public Coordinate Coordinates
         {
             get => coordinate;
@@ -87,27 +113,55 @@ namespace Fomin_SR4_var3
         }
     }
 
+    /// <summary>
+    ///     Класс SquareEventArgs для передачи конечной точки
+    /// </summary>
     class SquareEventArgs : EventArgs
     {
+        /// <summary>
+        ///     Конструктор, принимающий конечную точку
+        /// </summary>
+        /// <param name="end"></param>
         public SquareEventArgs(Coordinate end)
         {
             End = end;
         }
+        /// <summary>
+        ///     Конечная точка
+        /// </summary>
         public Coordinate End { get; set; }
     }
 
+    /// <summary>
+    ///     Класс Coordinate для удобного хранения точек
+    /// </summary>
     class Coordinate
     {
+        /// <summary>
+        ///     Первая координата
+        /// </summary>
         public int A { get; set; }
 
+        /// <summary>
+        ///     Вторая координата
+        /// </summary>        
         public int B { get; set; }
 
+        /// <summary>
+        ///     Конструктор
+        /// </summary>
+        /// <param name="a">первая координата</param>
+        /// <param name="b">вторая координата</param>
         public Coordinate(int a, int b)
         {
             A = a;
             B = b;
         }
 
+        /// <summary>
+        ///     Конструктор без параметров
+        ///     A = 0, B = 0
+        /// </summary>
         public Coordinate()
         {
             A = 0;
