f6852694c20402342be459d5c39c06383df68255
Sergey Fomin <sdfomin@edu.hse.ru>
2019-03-07 15:42:55 +0300

    Seminar08

---
+++ 3module/Seminar08/Task03/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    public interface IFigure
+    {
+        double Area();
+    }
+
+    class Square : IFigure
+    {
+        double IFigure.Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Class1
+    {
+        public void Main()
+        {
+
+        }
+    }
+}
