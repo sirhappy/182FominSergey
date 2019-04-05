4c64987eb3ed82852f262c58f75ee9c7025219ba
Sergey Fomin <sdfomin@edu.hse.ru>
2019-04-05 14:36:36 +0300

    4 модуль 1 семинар

---
+++ 4module/Seminar01/Seminar01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Seminar01
+{
+    [Serializable]
+    public class Student
+    {
+        public Student(string surname, int course)
+        {
+            Surname = surname;
+            Course = course;
+        }
+
+        public Student()
+        {
+            Surname = null;
+            Course = 0;
+        }
+
+        public string Surname { get; set; }
+        public int Course { get; set; }
+
+    }
+
+    [Serializable]
+    public class Group
+    {
+        public string Name { get; set; }
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public override string ToString()
+        {
+            return Name + " " + Students.Count.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group0 = new Group() { Name = "BPI182" };
+            Group group1 = new Group() { Name = "BPI181" };
+
+            group0.Students.Add(new Student("Fomin", 1));
+            group0.Students.Add(new Student("Petelin", 2));
+
+            group1.Students = group0.Students;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream("data0.bin", FileMode.Create))
+            {
+                bf.Serialize(fs, group0);
+            }
+
+            SoapFormatter sf = new SoapFormatter();
+            XmlSerializer xs = new XmlSerializer(typeof(Group));
+            using (FileStream fs = new FileStream("data1.xml", FileMode.Create))
+            {
+                xs.Serialize(fs, group1);
+            }
+
+            Group group0d;
+            using (FileStream fs = new FileStream("data0.bin", FileMode.Open))
+            {
+                group0d = (Group)bf.Deserialize(fs);
+            }
+
+            Console.WriteLine(group0d);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4module/Seminar01/Task02/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task02
+{
+
+    [Serializable]
+    public class Human
+    {
+        public string Name { get; set; }
+    }
+
+    [Serializable]
+    public class Professor : Human
+    {
+        
+    }
+
+    [Serializable]
+    public class Department
+    {
+        public string Name { get; set; } 
+        public List<Human> Employees { get; set; } = new List<Human>();
+    }
+    
+    [Serializable]
+    public class University
+    {
+        public string Name { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            University university = new University() { Name = "HSE", Departments = new List<Department>() };
+            university.Departments.Add(new Department() { Name = "Департамент" });
+
+            XmlSerializer xs = new XmlSerializer(typeof(University));
+            using (FileStream fs = new FileStream("universities.xml", FileMode.Create))
+            {
+                xs.Serialize(fs, university);
+            }
+
+            using (FileStream fs = new FileStream("universities.xml", FileMode.Open))
+            {
+                object o = xs.Deserialize(fs);
+                Console.WriteLine(o.GetType());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
